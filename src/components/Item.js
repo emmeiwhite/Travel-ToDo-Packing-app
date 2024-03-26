4e2ee776a3a4d89b31178ca76e8a8940e5d73d4a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-03-26 16:01:47 +0530

    Added a list in PackageList and did some styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Item() {
-  return <div>Item</div>;
+export default function Item({ description, quantity, id, packed }) {
+  return (
+    <li className={`${packed ? "packed" : ""}`}>
+      <span>
+        {quantity} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Item from "./Item";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 export default function PackingList() {
-  return <div className="list">LIST</div>;
+  const listItems = initialItems.map((item) => (
+    <Item
+      {...item}
+      key={item.id}
+    />
+  ));
+
+  return (
+    <div className="list">
+      <ul>{listItems}</ul>
+    </div>
+  );
 }
