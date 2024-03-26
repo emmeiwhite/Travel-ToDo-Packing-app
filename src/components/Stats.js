3bf8fbc33a69d9291743d867948f75eb25741252
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-03-26 23:31:42 +0530

    Derived State benefits

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function Stats({ totalItems, totalChecked }) {
+  if (totalItems === 0) {
+    return (
+      <p className="stats">
+        <em>Start adding items you plan to take for the trip 🚀</em>
+      </p>
+    );
+  }
   return (
     <footer className="stats">
       You have {totalItems} items on your list, and you already packed{" "}
